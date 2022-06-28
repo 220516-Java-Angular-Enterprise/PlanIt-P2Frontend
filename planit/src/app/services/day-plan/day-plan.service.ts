d2404a9b3dbd55f92cbbe09e696f9d7bba4f6c07
ML <labrecquemegan@hotmail.com>
2022-06-28 11:16:15 -0400

    homepage stash

--- planit/src/app/services/day-plan/day-plan.service.ts
+++ planit/src/app/services/day-plan/day-plan.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { Plans } from '../../models/plans';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DayPlanService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private planURL = "http://planitproject2-env-1.eba-tguhtqck.us-east-2.elasticbeanstalk.com/planit/";
+
+  getAllTrips(): Promise<Plans[]> {
+    return firstValueFrom(this.http.get<Plans[]>(this.planURL));
+  }
+
+  getTripById(id: string): Promise<Plans> {
+    return firstValueFrom(this.http.get<Plans>(this.planURL + "/" + id));
+  }
+
+  createNewTrip(plan: Plans) {
+    return firstValueFrom(this.http.post(this.planURL, plan));
+  }
+
 }
