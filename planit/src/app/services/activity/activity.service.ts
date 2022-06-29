2876c24a71f679818c78082c60a6ca5d6afc08ec
ML <labrecquemegan@hotmail.com>
2022-06-29 18:00:44 -0400

    created calls for getting activity by name

--- planit/src/app/services/activity/activity.service.ts
+++ planit/src/app/services/activity/activity.service.ts
@@ -15,38 +15,42 @@ export class ActivityService {
 
   constructor(private http: HttpClient) { }
 
+  locationByPlaceurl = 'https://trueway-places.p.rapidapi.com/FindPlacesNearby'
+  locationByNameurl = 'https://trueway-places.p.rapidapi.com/FindPlaceByText'
+
+ 
+
   options = {
     // method: 'GET',
     headers: {
       'X-RapidAPI-Key': '20996e2b6amshc0124e31bc6de18p1e05e5jsnd571f19596ce',
       'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
+    },
+    params: {
+      'location': '37.783366, -122.402325',
+      'type': 'cafe',
+      'radius': '150',
+      'language': 'en'
     }
   };
-  
-  activitydata() {
-  fetch('https://trueway-places.p.rapidapi.com/FindPlacesNearby?location={{this.lat}}%2C{{this.lng}}&type={{this.type}}&radius=150&language=en', this.options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-}
 
-    // activitydata(){
-    //     let headers=new HttpHeaders({
-    //       'X-RapidAPI-Key': '20996e2b6amshc0124e31bc6de18p1e05e5jsnd571f19596ce',
-    //       'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
-    //     });
-    //     this.http
-    //     .get<any>('https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&type=cafe&radius=150&language=en',{
-    //       headers:headers
-    //     })
-    //     .subscribe(data => {
-    //       console.log(data);
-    //     });
-    //   }
-
-    getActivityByLocation(): Observable<Activity[]>{
-        // console.log(this.http.get<Activity[]>('https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&type=cafe&radius=150&language=en', this.options));
-        return this.http.get<Activity[]>('https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=37.783366%2C-122.402325&type=cafe&radius=150&language=en', this.options)
+  nameOpt = {
+    // method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '20996e2b6amshc0124e31bc6de18p1e05e5jsnd571f19596ce',
+      'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
+    },
+    params: {
+      'text': 'Children\'s Creativity Museum',
+      'language': 'en'
     }
+  };
 
+    getActivityByLocal(): Observable<Activity[]>{
+      return this.http.get<Activity[]>(this.locationByPlaceurl, this.options)
+  }
+
+  getActivitybyName(): Observable<Activity[]>{
+    return this.http.get<Activity[]>(this.locationByNameurl, this.nameOpt)
+  }
 }
